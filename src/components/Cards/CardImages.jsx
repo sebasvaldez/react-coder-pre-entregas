e8d2c2afc570b41ad75491a2c9b54dbe0e0c08c0
sebasvaldez <marcelosebastianvaldez@gmail.com>
2023-06-22 19:36:43 -0300

    Mejorando el footer y los Links

--- src/components/Cards/CardImages.jsx
+++ src/components/Cards/CardImages.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import {Button, Card} from "react-bootstrap";
+import {Card} from "react-bootstrap";
 import "./CardImages.css";
 
 const CardImages = ({products}) => {
@@ -8,15 +8,18 @@ const CardImages = ({products}) => {
   return products.map(({id,url_imagen, nombre,stock,categoria_id})=>{
 
     return(
+      <Link to={`/item/${id}`} className="Link">
+      
       <Card style={{ width: "25rem", marginTop:"2rem" }} key={id}>
         <Card.Img variant="top" src={url_imagen} />
         <Card.Body>
           <Card.Title>{nombre}</Card.Title>
           <Card.Text>Unidades: {stock}</Card.Text>
           
-          <Link to={`/item/${id}`} variant="primary" className="Link btn btn-primary">Ver</Link>
+          
         </Card.Body>
       </Card>
+      </Link>
     );
   })
 };
